99886ed4929129447f7349bb9c026e58fe749563
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-09-19 18:11:00 +0430

    Add HelloWorldExample.js component

---
+++ src/docs/examples/HelloWorld/HelloWorldExample.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+/** Custom message */
+function HelloWorldExample() {
+  return <HelloWorld message="Universe" />;
+}
+
+export default HelloWorldExample;
