95f457a5545bef3011911e71ec8d20317659d5a9
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2022-09-20 13:56:41 +0430

    Add Label Atom

---
+++ src/components/Label/Label.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/** Field Label */
+function Label({ htmlFor, label, required }) {
+  return (
+    <label htmlFor={htmlFor} style={{ display: 'block' }}>
+      {label}
+      {required && <span style={{ color: 'red' }}>*</span>}
+    </label>
+  );
+}
+
+Label.propTypes = {
+  /** Value for HTML "for" attribute of label element */
+  htmlFor: PropTypes.string.isRequired,
+
+  /** Label value */
+  label: PropTypes.string.isRequired,
+
+  /** Specify required label with red asterisk */
+  required: PropTypes.bool,
+};
+
+Label.defaultProps = {
+  required: false,
+};
+
+export default Label;
